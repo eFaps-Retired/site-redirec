947fd491835cb804e5d0e9ae892485a6211fe369
tim.moxter <tim.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-10-30 00:53:33 +0000

    - first version of the efaps site redirect google appengine

---
+++ site-redirect/src/main/java/org/efaps/site_redirect/RedirectServlet.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2003 - 2009 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.site_redirect;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+public class RedirectServlet
+    extends HttpServlet
+{
+    /**
+     * Generated serial version UID of the eFaps redirect servlet.
+     */
+    private static final long serialVersionUID = -3515249212260192796L;
+
+    /**
+     * Default URL where to redirect if no defined server name in
+     * {@link #REDIRECTS} is found.
+     */
+    private static final String DEFAULT_URL = "http://efaps.googlecode.com";
+
+    /**
+     * Map defining all redirects from given server name to the http URL.
+     */
+    private static final Map<String, String> REDIRECTS = new HashMap<String, String>(8);
+    static {
+        RedirectServlet.REDIRECTS.put("www.efaps.org",                  RedirectServlet.DEFAULT_URL);
+        RedirectServlet.REDIRECTS.put("efaps-kernel.efaps.org",         "http://efaps.googlecode.com/svn/site/eFaps-kernel/index.html");
+        RedirectServlet.REDIRECTS.put("maven-java5.efaps.org",          "http://efaps.googlecode.com/svn/site/maven-java5/index.html");
+        RedirectServlet.REDIRECTS.put("maven-slf4jlogger.efaps.org",    "http://efaps.googlecode.com/svn/site/maven-slf4jlogger/index.html");
+        RedirectServlet.REDIRECTS.put("number2words.efaps.org",         "http://efaps.googlecode.com/svn/site/number2words/index.html");
+        RedirectServlet.REDIRECTS.put("source.efaps.org",               "http://code.google.com/p/efaps/source/browse");
+        RedirectServlet.REDIRECTS.put("wiki.efaps.org",                 "http://code.google.com/p/efaps/wiki");
+        RedirectServlet.REDIRECTS.put("issues.efaps.org",               "http://code.google.com/p/efaps/issues");
+    }
+
+    /**
+     * @param _request      http request
+     * @param _response     http response (to redirect)
+     * @throws IOException if redirect failed
+     */
+    @Override()
+    public void doGet(final HttpServletRequest _request,
+                      final HttpServletResponse _response)
+        throws IOException
+    {
+        final String newUrl = RedirectServlet.REDIRECTS.get(_request.getServerName());
+
+        if (newUrl != null)  {
+            _response.sendRedirect(newUrl);
+        } else  {
+            _response.sendRedirect(RedirectServlet.DEFAULT_URL);
+        }
+    }
+
+}
